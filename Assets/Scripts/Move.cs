98ccb8146847db35dbc70194af768e58134ed591
nishanlama <nishanlama03@gmail.com>
2020-05-07 13:52:51 -0700

    Github

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -9,10 +9,28 @@ public class Move : MonoBehaviour
     {
         
     }
-
+      
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Translate(speed);
+        Vector3 currentSpeed = Vector3.zero;
+
+          if (Input.GetKeyDown(KeyCode.A))
+        {
+            currentSpeed.x = -speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            currentSpeed.x = speed.x;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            currentSpeed.z= speed.z;
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            currentSpeed.z = -speed.z;
+        }
+        gameObject.transform.Translate(currentSpeed * Time.deltaTime);
     }
 }
