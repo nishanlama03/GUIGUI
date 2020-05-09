9105f7ec69bfbce45253fadb7821e63d9c0af849
nishanlama <nishanlama03@gmail.com>
2020-05-08 17:27:07 -0700

    created new script killbox and bullet

---
+++ Assets/Scripts/KillBox.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KillBox : MonoBehaviour
+{
+    public Transform spawnPoint;
+  public void OnCollisionEnter(Collision collision)
+  {
+
+  }
+    
+     public void OnCollisionExit(Collision collision)
+  {
+      
+  }
+   
+   
+    private void OnTriggerEnter(Collider other)
+  {
+      if ( other.gameObject.CompareTag("CapsulePlayer"))
+      {
+          Debug.Log("Player hit me!");
+          other.gameObject.transform.position = spawnPoint.position;
+      }
+  }
+   
+    private void OnTriggerExit(Collider other)
+  {
+      
+  }
+   
+}
--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
+    public GameObject bullet;
+
+    public Transform bulletSpawnPoint;
+
     public Vector3 speed;
 
     public float turnSpeed;
@@ -12,26 +16,26 @@ public class Move : MonoBehaviour
 
     private bool isJumping = false;
 
+    private float currentSpeed = 0.0f;
+     
+    private float distanceToGround = 0.0f;
+
+    private int jumpCount = 0;
+
     private Rigidbody rb;
 
 
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        distanceToGround = GetComponent<Collider>().bounds.extents.y;
+
     }
       
 
-       
-    
-  
     void Update()
     {
-      
-
-    }
-    void FixedUpdate()
-    {
-         float currentSpeed = 0.0f;
+        currentSpeed = 0.0f;
        float currentTurnAmount = 0.0f;
 
        if (Input.GetKey(KeyCode.A)) 
@@ -50,17 +54,43 @@ public class Move : MonoBehaviour
        {
            currentSpeed = -speed.x;
        }
-       
+
+       if (Input.GetKey(KeyCode.F))
+       {
+           GameObject newBullet = GameObject.Instantiate(bullet, bulletSpawnPoint.position, new Quaternion());
+           Rigidbody bulletBody = newBullet.GetComponent<Rigidbody>();
+           bulletBody.AddForce(transform.forward * 30, ForceMode.Impulse);
+       }
+
         gameObject.transform.Rotate(Vector3.up, currentTurnAmount * Time.deltaTime);
-      
+    }
+    
+    bool IsGrounded()
+    {
+        return Physics.Raycast(transform.position,Vector3.down, distanceToGround + 0.1f);
+    }
+    
+    void FixedUpdate()
+    {
         rb.AddForce(transform.forward * currentSpeed * Time.deltaTime,ForceMode.Impulse);
-  
-        if (Input.GetKeyUp(KeyCode.Space) && !isJumping)
+        
+    
+        bool isGrounded = IsGrounded();
+
+        if (isGrounded)
         {
-            isJumping = true;
+            jumpCount = 0;
+        }
+        
+        if (Input.GetKey(KeyCode.Space) && (isGrounded || jumpCount < 2))
+        {
             rb.AddForce(Vector3.up * jumpforce, ForceMode.Impulse);
+            jumpCount ++;
         }
         rb.angularVelocity = Vector3.zero;
+    }
+       
+        
 
-  }
-  }
+  
+}
