faf1fc2919e457a1586be54e396f3a7ee76d95a2
nishanlama <nishanlama03@gmail.com>
2020-05-16 12:55:14 -0700

    New Script Move2D and level construct

---
+++ Assets/Scripts/Move2d.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move2d : MonoBehaviour
+{
+    public float moveSpeed;
+     
+     public float jumpforce;
+
+     private Rigidbody2D rb;
+
+     
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D> ();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        float move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(move * moveSpeed, rb.velocity.y);
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb.velocity = new Vector2 (rb.velocity.x,jumpforce);
+        }
+
+
+
+
+
+    }
+}
